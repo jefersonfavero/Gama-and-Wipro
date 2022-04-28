d436f364478278187f9239c3d5650494517ebb76
jefersonfavero <jefersonvieirafavero@gmail.com>
2022-04-28 00:12:51 -0300

    New feature Swagger

--- ProjetoIntegrador/baseBank/src/test/java/dev/israelld/baseBank/BaseBankApplicationTests.java
+++ ProjetoIntegrador/baseBank/src/test/java/dev/israelld/baseBank/BaseBankApplicationTests.java
@@ -1,13 +1,16 @@
 package dev.israelld.baseBank;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BaseBankApplicationTests {
+public class BaseBankApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
+		assertTrue(true);
 	}
 
 }
